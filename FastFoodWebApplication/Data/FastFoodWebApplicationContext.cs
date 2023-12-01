9d9ef7752292345e400fcd70728a5e7dfe57364c
chicaoha <cao.ha.ha.set18@eiu.edu.vn>
2023-12-01 15:03:35 +0700

    update model Cao

---
+++ FastFoodWebApplication/Data/FastFoodWebApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FastFoodWebApplication.Models;
+
+namespace FastFoodWebApplication.Data
+{
+    public class FastFoodWebApplicationContext : DbContext
+    {
+        public FastFoodWebApplicationContext (DbContextOptions<FastFoodWebApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FastFoodWebApplication.Models.DishType> DishType { get; set; } = default!;
+
+        public DbSet<FastFoodWebApplication.Models.Dish> Dish { get; set; }
+    }
+}
