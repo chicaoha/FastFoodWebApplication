2b50b1e5bb23592a7b34eddba18553028a371b91
Phuong Anh <97769881+Phuonganh1307@users.noreply.github.com>
2023-12-01 16:27:53 +0700

    commit View Menu

--- FastFoodWebApplication/Controllers/MenuController.cs
+++ FastFoodWebApplication/Controllers/MenuController.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FastFoodWebApplication.Data;
+using FastFoodWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FastFoodWebApplication.Controllers
 {
     public class MenuController : Controller
     {
-        public IActionResult Index()
+        public readonly FastFoodWebApplicationContext _context;
+        public MenuController( FastFoodWebApplicationContext context)
         {
+ 
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+
+            List<Dish> dishes = await _context.Dish.ToListAsync();
+
+            ViewData["Dishes"] = dishes;
+
+            List<DishType> types = await _context.DishType.ToListAsync();
+
+            ViewData["DishType"] = types;
             return View();
         }
     }
