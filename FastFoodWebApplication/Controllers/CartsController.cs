d1e1108aff2e50bf50c8659248612f07bcefca86
Phuong Anh <97769881+Phuonganh1307@users.noreply.github.com>
2023-12-22 10:47:35 +0700

    commit updadte

--- FastFoodWebApplication/Controllers/CartsController.cs
+++ FastFoodWebApplication/Controllers/CartsController.cs
@@ -72,7 +72,7 @@ namespace FastFoodWebApplication.Controllers
         // POST: Carts/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-
+     
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("CartId,DishId,Quantity,size,price,UserId")]
